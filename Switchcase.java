46f8674fb30683657fe038f8420d46f3695e11fd
Atia Sanjida Rodela <sanjidaatia438@gmail.com>
2026-05-16 14:21:26 +0600

    Add files via upload

---
+++ Switchcase.java
@@ -0,0 +1,22 @@
+public class Switchcase {
+    public static void main(String[] args){
+        //switch statement
+        int i=6;
+        switch(i){
+            case 1:
+            System.out.println("i is 1");
+            break;
+            case 2:
+             System.out.println("i is 2");
+             break;
+             case 3:
+              System.out.println("i is 3");
+              break;
+              default:
+                 System.out.println("i>3");
+                 break;
+        }
+        }
+        }
+    
+
