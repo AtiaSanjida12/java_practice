a62c6bffe1c19748e3ed3bb566bac675ad44018d
Atia Sanjida Rodela <sanjidaatia438@gmail.com>
2026-06-05 19:13:32 +0600

    Add files via upload

---
+++ Variables.java
@@ -0,0 +1,16 @@
+public class Variables {
+    public static void main(String[] args){
+        int sumiNumber=60;
+        System.out.println(sumiNumber);
+        int safiNumber =87;
+        System.out.println(safiNumber);
+        float myFloat=5.0f;
+        System.out.println(myFloat);
+        double myDouble=4756.988;
+        System.out.println(myDouble);
+        String wishes="Good Morning";
+        System.out.println(wishes );
+        char myCharacter='k';
+        System.out.println(myCharacter);
+    }
+}
